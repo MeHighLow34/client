06fb1b81b1c10376467d376636942b72fedfb63c
Mihajlo Kucak <kucakmihajloskola@gmail.com>
2022-11-08 13:54:44 +0100

    littlechangemakesthedifference

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -86,7 +86,7 @@ const AppProvider = ({ children }) => {
   async function registerUser(currentUser) {
     dispatch({ type: REGISTER_USER_BEGIN });
     try {
-      const response = await axios.post("api/api/auth/register", currentUser);
+      const response = await axios.post("/api/api/auth/register", currentUser);
       const { user, token } = response.data;
       dispatch({ type: REGISTER_USER_SUCCESS, payload: { user, token } });
       addUserToLocalStorage({ user, token });
